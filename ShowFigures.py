3fd0eb9e1b94084128992e1fb860f32229e9f904
fdzdani <115399394+fdzdani@users.noreply.github.com>
2024-03-03 20:23:18 +0100

    Add files via upload

---
+++ ShowFigures.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import os
+import xml.etree.cElementTree as ET
+import textwrap
+
+xmlfiles = "./Output/"
+output = "./Wordclouds/"
+figuras = {}
+
+for xml in os.listdir(xmlfiles):
+    path = os.path.join(xmlfiles, xml)
+    if os.path.isfile(path):
+        with open(path, 'r') as file:
+            xml_data = file.read()
+        root = ET.fromstring(xml_data)
+        namespaces = {'tei':'http://www.tei-c.org/ns/1.0'}
+        figura = root.findall('.//tei:figure', namespaces)
+        xml = os.path.splitext(xml)[0].replace(".tei","").replace(".grobid", "")
+        figuras[xml] = len(figura)
+        
+archivo_pdf = "Figuras.pdf"
+path_pdf = os.path.join(output, archivo_pdf) 
+
+wrapped_labels = ['\n'.join(textwrap.wrap(label,20)) for label in figuras.keys()]        
+plt.bar(range(len(figuras)), list(figuras.values()), align='center')
+plt.xticks(range(len(figuras)), wrapped_labels, rotation=90)
+plt.tight_layout()
+
+plt.savefig(path_pdf, bbox_inches='tight', format='pdf')
+plt.close()
---
+++ WordCloud.py
@@ -0,0 +1,32 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import os
+import xml.etree.cElementTree as ET
+
+xmlfiles = "./Output/"
+output = "./Wordclouds/"
+for xml in os.listdir(xmlfiles):
+    path = os.path.join(xmlfiles, xml)
+    if os.path.isfile(path):
+        with open(path, 'r') as file:
+            xml_data = file.read()
+        root = ET.fromstring(xml_data)
+        namespaces = {'tei':'http://www.tei-c.org/ns/1.0'}
+        abstracts = root.findall('.//tei:abstract', namespaces)
+        for abstract in abstracts:
+            div = abstract.find('tei:div', namespaces)
+            p = div.find('tei:p', namespaces)
+            abstract_info = p.text
+        wordcloud = WordCloud(width = 800, height = 800, background_color = 'white', 
+                      stopwords = None, min_font_size = 10).generate(abstract_info)
+        archivo = os.path.splitext(xml)[0].replace(".tei","")
+        archivo_pdf = archivo + ".wordcloud.pdf"
+        path_pdf = os.path.join(output, archivo_pdf)
+        plt.figure(figsize = (8, 8), facecolor = None)
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.tight_layout(pad = 0)
+
+        plt.title(xml)
+        plt.savefig(path_pdf, format='pdf')
+        plt.close()
